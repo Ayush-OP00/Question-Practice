7a31f205baf627fe8eefa1b275347bb8eea93fc0
Ayush-OP00 <allinone7925@gmail.com>
2026-02-25 11:04:18 +0530

    This commit consists a program to display the version of python being used and the information related to the version

---
+++ version_checker.py
@@ -0,0 +1,9 @@
+import sys #used to access system specific parameters and functions
+
+print("python version")
+
+print(sys.version) #prints the version of python being used
+
+print("version information") #prints the version info of python being used
+
+print(sys.version_info) #prints the version info of python being used in a tuple format
