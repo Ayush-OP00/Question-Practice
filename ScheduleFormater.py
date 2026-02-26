08d7cf1037d7ba19bda256b1bd00b44d53613f53
Ayush-OP00 <allinone7925@gmail.com>
2026-02-26 12:56:30 +0530

    This commit changes the format of the text entered in the input

---
+++ ScheduleFormater.py
@@ -0,0 +1,5 @@
+exam_st_date = (4, 3, 2026)
+
+# Print the exam start date using string formatting
+# The '%i' placeholders are filled with the values from the 'exam_st_date' tuple
+print("The examination will start from : %i / %i / %i" % exam_st_date)
