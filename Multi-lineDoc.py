42eb001ee58b05990d4633755dbd14d4d3e95ad0
Ayush-OP00 <allinone7925@gmail.com>
2026-02-27 12:01:39 +0530

    Just a small program depicting how to print multi-line comments in python

---
+++ Multi-lineDoc.py
@@ -0,0 +1,7 @@
+#Use triple quotes ("""") to print multo-line strings in Python. This allows you to include line breaks and special characters without needing to escape them.
+print ("""
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+       """)
