11da5c0fa7bfdee206a367244be52c1908a36b61
Ayush-OP00 <allinone7925@gmail.com>
2026-02-27 10:22:31 +0530

    This commit contains the code to give the docstring of any function, module etc.

---
+++ FunctionDocumentation.py
@@ -0,0 +1,3 @@
+#__doc__ is a special attribute that contains the documentation string of the function. In this case, it will print the documentation for the built-in abs() function, which returns the absolute value of a number.
+
+print(abs.__doc__)
