4f62c3c59f478b2cc4e5e64483a16828665f104a
Ayush-OP00 <allinone7925@gmail.com>
2026-02-27 19:49:57 +0530

    This is a commiit which consists of the code that adds a string "Is" to the sentence if the sentence doesn't start with one

---
+++ StringModifier.py
@@ -0,0 +1,11 @@
+#program to get a newly-generated string from a given string where "Is" has been added to the front. Return the string unchanged if the given string already begins with "Is".
+
+def add_is(s):
+    if s.startswith("Is"): # The 'startswith' method is used to check if the string 's' starts with the substring "Is". If it does, the function returns the original string 's' unchanged.
+        return s
+    else:
+        return "Is " + s
+    
+string = input("Enter a string: ")
+result = add_is(string)
+print (result)
