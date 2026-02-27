9917657b65b921509e92d97d1f763c49edf7afac
Ayush-OP00 <allinone7925@gmail.com>
2026-02-27 19:59:35 +0530

    just a liitle modification

--- StringModifier.py
+++ StringModifier.py
@@ -1,7 +1,8 @@
 #program to get a newly-generated string from a given string where "Is" has been added to the front. Return the string unchanged if the given string already begins with "Is".
 
 def add_is(s):
-    if s.startswith("Is"): # The 'startswith' method is used to check if the string 's' starts with the substring "Is". If it does, the function returns the original string 's' unchanged.
+    if s.startswith("Is"): 
+        # The 'startswith' method is used to check if the string 's' starts with the substring "Is". If it does, the function returns the original string 's' unchanged.
         return s
     else:
         return "Is " + s
