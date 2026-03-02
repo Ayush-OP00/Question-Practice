218947af1e9fca781eb963d94b632391d714dd6c
Ayush-OP00 <allinone7925@gmail.com>
2026-03-02 19:36:07 +0530

    Added a new function to determine the occurence of that specific number in the group.

--- ElementSearch.py
+++ ElementSearch.py
@@ -7,9 +7,23 @@ def search_num(num, group):
     else:
         return "False"
     
+#Little touch to determine the occurence of the particular number in group of numbers.
+
+def count(num, group):
+    count = 0
+
+    for n in group:
+         if n == num:
+            count = count + 1
+    return count
+    
+    
 number = [int(x) for x in input("Enter the group of numbers: ").split()] 
 #taking input in the form of list of integers from the user and storing it in the variable 'number'
 num = int(input("Enter the number to search: "))
 result = search_num(num, number)
+occurence = count(num, number)
 
+print("The number of occurrences of", num, "is:", occurence)
 print("Is the number in the group?", result)
+
