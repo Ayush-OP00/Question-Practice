2aa74e3397b13cca2048d3605dba92c585c46a84
Ayush-OP00 <allinone7925@gmail.com>
2026-03-04 23:53:49 +0530

    This commit returns the Greatest Common Factot between the two numbers.

---
+++ GCDCalculator.py
@@ -0,0 +1,23 @@
+#program that computes the greatest common divisor (GCD) of two positive integers.
+
+def gcd(a, b):
+    
+    gcd = 1
+
+    if a % b == 0:
+        return b
+    # Iterate from half of b down to 1.
+    for k in range(int(b / 2), 0, -1):
+        # Check if both a and b are divisible by k.
+        if a % k == 0 and b % k == 0:
+            # Update the GCD to the current value of k and exit the loop.
+            gcd = k
+            break
+    
+    # Return the calculated GCD.
+    return gcd
+
+num1 = int(input("Enter the first positive integer: "))
+num2 = int(input("Enter the second positive integer: "))
+result = gcd(num1, num2)
+print("The GCD of", num1, "and", num2, "is", result)
