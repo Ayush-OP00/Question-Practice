b58431e76d5cc3ac7d98c88e07683602eae2d20f
Ayush-OP00 <allinone7925@gmail.com>
2026-03-06 00:26:54 +0530

    This commit includes the writing of comments to explain the process of finding GCD.

--- GCDCalculator.py
+++ GCDCalculator.py
@@ -4,10 +4,15 @@ def gcd(a, b):
     
     gcd = 1
 
-    if a % b == 0:
+    if a % b == 0:  # If a is divisible by b, then b is the GCD.
         return b
+    
     # Iterate from half of b down to 1.
-    for k in range(int(b / 2), 0, -1):
+
+    for k in range(int(b / 2), 0, -1): 
+        #range() function generates numbers for the loop: range(start, stop, step). 
+        # Loop counts backwards.
+        #-1 means: Decrement the loop variable k by 1 in each iteration, effectively counting downwards.
         # Check if both a and b are divisible by k.
         if a % k == 0 and b % k == 0:
             # Update the GCD to the current value of k and exit the loop.
