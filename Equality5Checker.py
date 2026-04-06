e075d66ede9fd9f1746dc93f980ee9d3e6376705
Ayush-OP00 <allinone7925@gmail.com>
2026-04-06 12:11:45 +0530

    This commit show the  above code in an optimised manner removing if-else statements from the code

--- Equality5Checker.py
+++ Equality5Checker.py
@@ -1,10 +1,13 @@
 #Python program that returns true if the two given integer values are equal or their sum or difference is 5.
-def equality_checker(x, y):
-    if x == y  or abs(x + y) == 5 or abs(x - y) == 5:
-        return True
-    else:
-        print("not the expected input")
+# def equality_checker(x, y):
+#     if x == y  or abs(x + y) == 5 or abs(x - y) == 5:
+#         return True
+#     else:
+#         print("not the expected input")
 
-x, y = map(int, input("Enter two no. separated by space:").split())
-result = equality_checker(x, y)
-print(result)
+# x, y = map(int, input("Enter two no. separated by space:").split())
+# result = equality_checker(x, y)
+# print(result)
+
+x, y = map(int, input("Enter two numbers: ").split())
+print(x == y or abs(x + y) == 5 or abs(x - y) == 5)
